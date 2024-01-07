b12e28e0b98647f77e799e6c2639910233b87692
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-01-08 03:24:15 +0530

    adding of assessment oview section

---
+++ src/Components/AssmntOverview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AssmntOverview = () => {
+  return (
+    <div className='px-[20px]'>
+        This is assessment overview section
+    </div>
+  )
+}
+
+export default AssmntOverview
--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from '../Components/Nav'
 import Assessment from '../Components/Assessment'
+import AssmntOverview from './AssmntOverview'
 const Main = () => {
   return (
     <div className='bg-white w-full'>
         <Nav/>
         <Assessment/>
+        <AssmntOverview/>
     </div>
   )
 }
