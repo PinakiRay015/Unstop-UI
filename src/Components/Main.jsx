113ff0d1fa19b883d290a24377baf3f41831270a
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-01-07 20:48:19 +0530

    nav bar section

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import Nav from '../Components/Nav'
+import Assessment from '../Components/Assessment'
 const Main = () => {
   return (
-    <div className='bg-white w-full'>
-        This is assessment section
+    <div className='bg-white w-full px-3'>
+        <Nav/>
+        <Assessment/>
     </div>
   )
 }
--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import React from "react";
 
 const Nav = () => {
   return (
-    <div>
-      This is nav bar
+    <div className="border-b border-zinc-300 h-[70px] flex items-center">
+      <div className="flex items-center justify-between w-full h-full">
+        <div className="flex items-center">
+          <h3 className="font-semibold text-[20px] text-[#1C4980] border-r-2 border-zinc-300 h-full px-4 py-2">
+            Assessment
+          </h3>
+          <h4 className="font-semibold text-[14px] border-b-2 border-[#0073E6] py-6 px-4 text-[#0073E6]">My Assessment</h4>
+        </div>
+        <div>
+          <img src="Assets/phone.svg" alt="" />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Nav
+export default Nav;
