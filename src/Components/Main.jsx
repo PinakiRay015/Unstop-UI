c6b6e6f09794262804e1c850ed1e69328cca8b57
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-01-09 00:13:27 +0530

    adding new assessment button feature

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Nav from '../Components/Nav'
 import Assessment from '../Components/Assessment'
 import AssmntOverview from './AssmntOverview'
 const Main = () => {
+
+  const [popup, setPopup] = useState("hidden")
+  const assessment = () => {
+    setPopup("block")
+  };
+
   return (
     <div className='bg-white w-full'>
         <Nav/>
         <Assessment/>
-        <AssmntOverview/>
+        <AssmntOverview assessment = {assessment} />
+        <div className='flex justify-center'>
+        <div className={`absolute top-0 bg-green-500 w-[591px] h-[766px] border border-black`}></div>
+        </div>
     </div>
   )
 }
