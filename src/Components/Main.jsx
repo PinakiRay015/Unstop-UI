f7ae6253a2a4670597918401196cd8f47da0ab51
Console.log("Pinaki") <Pinaki.bpd2001@gmail.com>
2024-01-11 16:56:29 +0530

    add assessment box

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -15,7 +15,12 @@ const Main = () => {
         <Assessment/>
         <AssmntOverview assessment = {assessment} />
         <div className='flex justify-center'>
-        <div className={`absolute top-0 bg-green-500 w-[591px] h-[766px] border border-black`}></div>
+        <div className={`absolute top-0 bg-[#FFFFFF] w-[591px] h-[766px] border border-black rounded-[15px]`}>
+          <nav className='flex justify-between px-[30px] py-[30px] border-b-2 border-[#DADCE0]'>
+            <p className='text-[#1C4980] font-semibold text-[20px]'>Create new assessment</p>
+            <img src="Assets/cross.svg" alt="" />
+          </nav>
+        </div>
         </div>
     </div>
   )
